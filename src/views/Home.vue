310b40afbbb07149e6049c52770f84ae619a6b48
iiiihua <iiiihua@163.com>
2020-10-29 02:52:23 +0800

    更新链接

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,7 +6,7 @@
                 <h1>free-ui</h1>
                 <h2>一个自由的ui框架</h2>
                 <p class="actions">
-                    <a href="https://github.com/iiiihua/year-ui.git">Github</a>
+                    <a href="https://github.com/iiiihua/free-ui.git">Github</a>
                     <router-link to="/doc">开始</router-link>
                 </p>
             </div>
