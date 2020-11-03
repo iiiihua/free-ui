c0aaa4bbd912cd58991b4a2966a4957b45b9f30a
iiiihua <iiiihua@163.com>
2020-11-03 14:18:35 +0800

    修复dialog无法正常显示标题与内容的bug

--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -1,33 +1,35 @@
 <template>
     <template v-if="visible">
-        <div class="gulu-dialog-overlay" @click="onClickOverLay" ref="ononon"></div>
-        <div class="gulu-dialog-wrapper">
-            <div class="gulu-dialog">
-                <header>{{tittle}}<span class="gulu-dialog-close" @click="close"></span></header>
-                <main>
-                    <slot/>
-                </main>
-                <footer>
-                    <Button level="main" @click="ok">ok</Button>
-                    <Button @click="cancel">cancel</Button>
-                </footer>
+        <Teleport to="body">
+            <div class="gulu-dialog-overlay" @click="onClickOverlay"></div>
+            <div class="gulu-dialog-wrapper">
+                <div class="gulu-dialog">
+                    <header>
+                        <slot name="title" />
+                        <span @click="close" class="gulu-dialog-close"></span>
+                    </header>
+                    <main>
+                        <slot name="content" />
+                    </main>
+                    <footer>
+                        <Button level="main" @click="ok">OK</Button>
+                        <Button @click="cancel">Cancel</Button>
+                    </footer>
+                </div>
             </div>
-        </div>
+        </Teleport>
     </template>
 </template>
 
 <script lang="ts">
     import Button from "./Button.vue";
-
     export default {
-        name: "Dialog",
-        components: {Button},
         props: {
             visible: {
                 type: Boolean,
                 default: false
             },
-            closeonClickOverLay: {
+            closeOnClickOverlay: {
                 type: Boolean,
                 default: true
             },
@@ -36,33 +38,37 @@
             },
             cancel: {
                 type: Function
-            },
-            tittle: {
-                type: String,
-                default: '提示标题'
             }
+        },
+        components: {
+            Button,
         },
         setup(props, context) {
             const close = () => {
                 context.emit('update:visible', false)
             }
-            const onClickOverLay = () => {
-                if (props.closeonClickOverLay) {
+            const onClickOverlay = () => {
+                if (props.closeOnClickOverlay) {
                     close()
                 }
             }
             const ok = () => {
-                if (props.ok?.() !== false)
+                if (props.ok?.() !== false) {
                     close()
+                }
             }
             const cancel = () => {
-                context.emit('cancel')
+                props.cancel?.()
                 close()
-
             }
-            return {close, onClickOverLay, ok, cancel}
+            return {
+                close,
+                onClickOverlay,
+                ok,
+                cancel
+            }
         }
-    }
+    };
 </script>
 
 <style lang="scss">
@@ -74,7 +80,6 @@
         box-shadow: 0 0 3px fade_out(black, 0.5);
         min-width: 15em;
         max-width: 90%;
-
         &-overlay {
             position: fixed;
             top: 0;
@@ -84,7 +89,6 @@
             background: fade_out(black, 0.5);
             z-index: 10;
         }
-
         &-wrapper {
             position: fixed;
             left: 50%;
@@ -92,8 +96,7 @@
             transform: translate(-50%, -50%);
             z-index: 11;
         }
-
-        > header {
+        >header {
             padding: 12px 16px;
             border-bottom: 1px solid $border-color;
             display: flex;
@@ -101,24 +104,20 @@
             justify-content: space-between;
             font-size: 20px;
         }
-
-        > main {
+        >main {
             padding: 12px 16px;
         }
-
-        > footer {
+        >footer {
             border-top: 1px solid $border-color;
             padding: 12px 16px;
             text-align: right;
         }
-
         &-close {
             position: relative;
             display: inline-block;
             width: 16px;
             height: 16px;
             cursor: pointer;
-
             &::before,
             &::after {
                 content: '';
@@ -129,15 +128,12 @@
                 top: 50%;
                 left: 50%;
             }
-
             &::before {
                 transform: translate(-50%, -50%) rotate(-45deg);
             }
-
             &::after {
                 transform: translate(-50%, -50%) rotate(45deg);
             }
         }
     }
-
 </style>
